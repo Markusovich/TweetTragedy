2e1e16bcb3934deba5428b5fc6129f56b22c3beb
Simon Markus <52682568+Markusovich@users.noreply.github.com>
2021-01-05 17:19:07 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -7,64 +7,77 @@ import json
 from pymongo import MongoClient
 import pymongo
 import logging
-from datetime import datetime
-from multiprocessing import Process
+import multiprocessing
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+
+# Link to Simon's database in mongodb atlas
+MONGO_HOST = 'mongodb+srv://markusovich:Alexmom99@cluster0.enna3.mongodb.net/twitterdb?retryWrites=true&w=majority'
+# Created a database named "twitterdb" in custer0
+
+CONSUMER_KEY = "Duko8zJpuAMqAqOCGn3gLCdU7"
+CONSUMER_SECRET = "K116dYN4lj0Ol4DGUsiCMAH8Vhz5tL02k3OPERWifGmwDLJ2rm"
+ACCESS_TOKEN = "603210098-mkYuAJoQ5SptpC7laiENIFeyquRwCqPfA2XRTgt0"
+ACCESS_TOKEN_SECRET = "vMeTce7nJffxEFDOGyNHrCHYIYIARP0IVywjaHPhg6oN8"
+
+auth = tw.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tw.API(auth, wait_on_rate_limit=True,
+             wait_on_rate_limit_notify=True)
+
+
+def get_tweets(searchWord, location, date1, date2):
+
+    searchWord += " -filter:retweets" + " -filter:replies"
+
+    # Collect tweets
+    tweets = tw.Cursor(api.search,
+                       q=searchWord,
+                       lang="en",
+                       result_type="recent",
+                       since=date1,
+                       until=date2).items(1)
+
+    client = MongoClient(MONGO_HOST)
+    db = client.twitterdb
+
+    # Iterate and print tweets
+    for tweet in tweets:
+        if location:
+            if location in tweet._json['user']['location']:
+                db.tweets.insert_one(tweet._json)
+        else:
+            db.tweets.insert_one(tweet._json)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
+
+        MONGO_HOST = 'mongodb+srv://markusovich:Alexmom99@cluster0.enna3.mongodb.net/twitterdb?retryWrites=true&w=majority'
+
         disasterName = request.form['searchDisaster']
         locationName = request.form['searchLocation']
         dateRange = request.form['daterange']
 
-        dateRange = re.split(' - |/', dateRange)
-        beginDate = dateRange[2] + "-" + dateRange[0] + "-" + dateRange[1]
-        endDate = dateRange[5] + "-" + dateRange[3] + "-" + dateRange[4]
-
-        beginDate = datetime.strptime(beginDate, '%Y-%m-%d')
-        endDate = datetime.strptime(endDate, '%Y-%m-%d')
+        beginDate = datetime.today() - timedelta(days=int(dateRange))
+        endDate = datetime.today()
         middleDate = beginDate + (endDate - beginDate)/2
+        beginDate = beginDate.strftime('%Y-%m-%d')
+        middleDate = middleDate.strftime('%Y-%m-%d')
+        endDate = endDate.strftime('%Y-%m-%d')
 
-        # Link to Simon's database in mongodb atlas
-        MONGO_HOST = 'mongodb+srv://markusovich:Alexmom99@cluster0.enna3.mongodb.net/twitterdb?retryWrites=true&w=majority'
-        # Created a database named "twitterdb" in custer0
+        p1 = multiprocessing.Process(target=get_tweets, args=(
+            disasterName, locationName, beginDate, middleDate))
+        p2 = multiprocessing.Process(target=get_tweets, args=(
+            disasterName, locationName, middleDate, endDate))
 
-        CONSUMER_KEY = "Duko8zJpuAMqAqOCGn3gLCdU7"
-        CONSUMER_SECRET = "K116dYN4lj0Ol4DGUsiCMAH8Vhz5tL02k3OPERWifGmwDLJ2rm"
-        ACCESS_TOKEN = "603210098-mkYuAJoQ5SptpC7laiENIFeyquRwCqPfA2XRTgt0"
-        ACCESS_TOKEN_SECRET = "vMeTce7nJffxEFDOGyNHrCHYIYIARP0IVywjaHPhg6oN8"
+        p1.start()
+        p2.start()
 
-        auth = tw.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-        api = tw.API(auth, wait_on_rate_limit=True,
-                     wait_on_rate_limit_notify=True)
-
-        def get_tweets(searchWord, date1, date2):
-
-            searchWord += " -filter:retweets" + " -filter:replies"
-
-            # Collect tweets
-            tweets = tw.Cursor(api.search,
-                               q=searchWord,
-                               lang="en",
-                               since=date1,
-                               until=date2).items(10)
-
-            client = MongoClient(MONGO_HOST)
-            db = client.twitterdb
-
-            # Iterate and print tweets
-            for tweet in tweets:
-                # if locationName in tweet._json['user']['location']:
-                db.tweets.insert_one(tweet._json)
-
-        t1 = Process(target=get_tweets(
-            disasterName, beginDate, middleDate)).start()
-        t2 = Process(target=get_tweets(
-            disasterName, middleDate, endDate)).start()
+        p1.join()
+        p2.join()
 
         return render_template('home.html')
     else:
