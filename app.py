d1ea44b62ce2f49a2bcba200c685d7be20875fe6
Simon Markus <52682568+Markusovich@users.noreply.github.com>
2021-01-06 00:00:48 -0600

    Add files via upload
    Added machine learning and multiprocessing for twitter API tweet retrieval.

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import pymongo
 import logging
 import multiprocessing
 from datetime import datetime, timedelta
+import pickle
 
 app = Flask(__name__)
 
@@ -26,6 +27,22 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tw.API(auth, wait_on_rate_limit=True,
              wait_on_rate_limit_notify=True)
 
+parser = pickle.load(open('parser.sav', 'rb'))
+
+
+def model_prediction(text):
+    true = 0
+    false = 0
+    for item in os.listdir("models/"):
+        model = pickle.load(open("models/{}".format(item), 'rb'))
+        prediction = model.predict(parser.transform([text]))[0]
+        if prediction == "T":
+            true += 1
+        else:
+            false += 1
+
+    return true > false
+
 
 def get_tweets(searchWord, location, date1, date2):
 
@@ -37,7 +54,7 @@ def get_tweets(searchWord, location, date1, date2):
                        lang="en",
                        result_type="recent",
                        since=date1,
-                       until=date2).items(1)
+                       until=date2).items(500)
 
     client = MongoClient(MONGO_HOST)
     db = client.twitterdb
@@ -46,9 +63,11 @@ def get_tweets(searchWord, location, date1, date2):
     for tweet in tweets:
         if location:
             if location in tweet._json['user']['location']:
-                db.tweets.insert_one(tweet._json)
+                if model_prediction(tweet._json['text']):
+                    db.tweets.insert_one(tweet._json)
         else:
-            db.tweets.insert_one(tweet._json)
+            if model_prediction(tweet._json['text']):
+                db.tweets.insert_one(tweet._json)
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -63,23 +82,97 @@ def home():
 
         beginDate = datetime.today() - timedelta(days=int(dateRange))
         endDate = datetime.today()
+        firstquarterDate = beginDate + (endDate - beginDate)/4
+        firstthirdDate = beginDate + (endDate - beginDate)/3
         middleDate = beginDate + (endDate - beginDate)/2
+        secondthirdDate = beginDate + (endDate - beginDate)/3 *2
+        thirdquarterDate = beginDate + (endDate - beginDate)/4 * 3
+
         beginDate = beginDate.strftime('%Y-%m-%d')
+        firstquarterDate = firstquarterDate.strftime('%Y-%m-%d')
+        firstthirdDate = firstthirdDate.strftime('%Y-%m-%d')
         middleDate = middleDate.strftime('%Y-%m-%d')
+        secondthirdDate = secondthirdDate.strftime('%Y-%m-%d')
+        thirdquarterDate = thirdquarterDate.strftime('%Y-%m-%d')
         endDate = endDate.strftime('%Y-%m-%d')
 
-        p1 = multiprocessing.Process(target=get_tweets, args=(
-            disasterName, locationName, beginDate, middleDate))
-        p2 = multiprocessing.Process(target=get_tweets, args=(
-            disasterName, locationName, middleDate, endDate))
+        if dateRange == 2:
+            p1 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, beginDate, endDate))
+            p1.start()
+            p1.join()
+        if dateRange == 3:
+            p1 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, beginDate, middleDate))
+            p2 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, middleDate, endDate))
+            p1.start()
+            p2.start()
+            p1.join()
+            p2.join()
+        if dateRange == 4:
+            p1 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, beginDate, middleDate))
+            p2 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, middleDate, endDate))
+            p1.start()
+            p2.start()
+            p1.join()
+            p2.join()
+        if dateRange == 5:
+            p1 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, beginDate, middleDate))
+            p2 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, middleDate, endDate))
+            p1.start()
+            p2.start()
+            p1.join()
+            p2.join()
+        if dateRange == 6:
+            p1 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, beginDate, firstthirdDate))
+            p2 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, firstthirdDate, secondthirdDate))
+            p3 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, secondthirdDate, endDate))
+            p1.start()
+            p2.start()
+            p3.start()
+            p1.join()
+            p2.join()
+            p3.join()
+        if dateRange == 7:
+            p1 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, beginDate, firstthirdDate))
+            p2 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, firstthirdDate, secondthirdDate))
+            p3 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, secondthirdDate, endDate))
+            p1.start()
+            p2.start()
+            p3.start()
+            p1.join()
+            p2.join()
+            p3.join()
+        else:
+            p1 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, beginDate, firstquarterDate))
+            p2 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, firstquarterDate, middleDate))
+            p3 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, middleDate, thirdquarterDate))
+            p4 = multiprocessing.Process(target=get_tweets, args=(
+                disasterName, locationName, thirdquarterDate, endDate))
+            p1.start()
+            p2.start()
+            p3.start()
+            p4.start()
+            p1.join()
+            p2.join()
+            p3.join()
+            p4.join()
 
-        p1.start()
-        p2.start()
-
-        p1.join()
-        p2.join()
-
-        return render_template('home.html')
+        return render_template('home.html', beginDate=beginDate, firstquarterDate=firstquarterDate, middleDate=middleDate, thirdquarterDate=thirdquarterDate, endDate=endDate)
     else:
         return render_template('home.html')
 
